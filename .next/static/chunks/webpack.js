1e6a3ba764d1e965591e7deefb9a142ba341a413
Arnish-B <arnish.2020@vitstudent.ac.in>
2022-12-30 20:55:17 +0530

    changes final

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8bad4e798e2a1ca"; }
+/******/ 		__webpack_require__.h = function() { return "02c1977ed8630bd8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
