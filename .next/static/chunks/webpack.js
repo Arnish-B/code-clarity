5dc7d20d842307b5e26351af52c2d202a2255089
Arnish-B <arnish.2020@vitstudent.ac.in>
2023-01-05 22:36:38 +0530

    errors fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "023b023288a17693"; }
+/******/ 		__webpack_require__.h = function() { return "f4c69a10bd1805f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
